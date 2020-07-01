e580b86a03c91e08de083d129ff42f9d7672c7c6
divya1357 <67141549+divya1357@users.noreply.github.com>
2020-07-01 15:14:22 +0530

    Update 321810304051-dic.py

--- 321810304051-dic.py
+++ 321810304051-dic.py
@@ -32,3 +32,16 @@ for color_key , value in d.items() :
 my_dict = {'d1' : 150 , 'd2' : 25 , 'd3' : -25}
 print(sum(my_dict.values()))
 
+#     7.     Write a Python script to concatenate following dictionaries to create a new one.       Sample Dictionary : 
+#   a. dic1={1:10, 2:20} 
+#   b. dic2={3:30, 4:40} 
+#   c. dic3={5:50,6:60} 
+#   Expected Result : {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60} 
+
+dic1 = {1:10, 2:20} 
+dic2 = {3:30, 4:40} 
+dic3 = {5:50,6:60} 
+dic4 = { }
+for d in (dic1 , dic2 , dic3) : dic4.update(d)
+print(dic4)
+
